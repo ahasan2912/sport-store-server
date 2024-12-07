f7746cb1ee2470342eecbb93a7de585530de9fa4
ahasan2912 <habib15-4284@diu.edu.bd>
2024-12-07 11:46:52 +0600

    added delete method for spacific equipment

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/equipment/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await sportCollection.deleteOne(query);
+      res.send(result);
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
